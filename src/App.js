21575cceeba423a387a0373ba5bd04adfc6674ed
Ryan Ross <ryan@reross.com>
2022-03-16 21:42:06 -0500

    added login feature, done with tutorial. for future: logout and infinite scroll

--- src/App.js
+++ src/App.js
@@ -1,21 +1,18 @@
 import "./App.css";
 import { ChatEngine } from "react-chat-engine";
 import ChatFeed from "./components/ChatFeed";
-import MessageForm from "./components/MessageForm";
-import MyMessage from "./components/MyMessage";
-import TheirMessage from "./components/TheirMessage";
+import Login from "./components/Login";
 
 const App = () => {
+  if (!localStorage.getItem("username")) return <Login />;
   return (
-    <div className="App">
-      <ChatEngine
-        height="100vh"
-        projectID="96c92568-a32a-46c6-8736-4678a69f6060"
-        userName="reross"
-        userSecret="123123"
-        renderChatFeed={(chatAppProps) => <ChatFeed {...chatAppProps} />}
-      />
-    </div>
+    <ChatEngine
+      height="100vh"
+      projectID="96c92568-a32a-46c6-8736-4678a69f6060"
+      userName={localStorage.getItem("username")}
+      userSecret={localStorage.getItem("password")}
+      renderChatFeed={(chatAppProps) => <ChatFeed {...chatAppProps} />}
+    />
   );
 };
 
---
+++ src/components/Login.jsx
@@ -0,0 +1,63 @@
+import React, { useState } from "react";
+import axios from "axios";
+
+const Login = () => {
+  const [userName, setUserName] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState("");
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const authObject = {
+      "Project-ID": "96c92568-a32a-46c6-8736-4678a69f6060",
+      "User-Name": userName,
+      "User-Secret": password,
+    };
+    try {
+      await axios.get("https://api.chatengine.io/chats", {
+        headers: authObject,
+      });
+
+      localStorage.setItem("username", userName);
+      localStorage.setItem("password", password);
+
+      window.location.reload();
+      setError("");
+    } catch (err) {
+      setError("Oops, incorrect credentials.");
+    }
+  };
+  return (
+    <div className="wrapper">
+      <div className="form">
+        <h1 className="title">Chat Application</h1>
+        <form onSubmit={handleSubmit}>
+          <input
+            type="text"
+            value={userName}
+            onChange={(e) => setUserName(e.target.value)}
+            className="input"
+            placeholder="Username"
+            required
+          />
+          <input
+            type="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            className="input"
+            placeholder="Password"
+            required
+          />
+          <div align="center">
+            <button type="submit" className="button">
+              <span>Start chatting</span>
+            </button>
+          </div>
+        </form>
+        <h1>{error}</h1>
+      </div>
+    </div>
+  );
+};
+
+export default Login;
