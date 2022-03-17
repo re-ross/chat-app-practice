9788c15050556ac942c418eb946577582aa200ea
Ryan Ross <ryan@reross.com>
2022-03-16 20:39:39 -0500

    added ChatForm component and skeleton for MessageForm, MyMessage, and TheirMessage

--- src/App.js
+++ src/App.js
@@ -1,7 +1,22 @@
 import "./App.css";
+import { ChatEngine } from "react-chat-engine";
+import ChatFeed from "./components/ChatFeed";
+import MessageForm from "./components/MessageForm";
+import MyMessage from "./components/MyMessage";
+import TheirMessage from "./components/TheirMessage";
 
-function App() {
-  return <div className="App"></div>;
-}
+const App = () => {
+  return (
+    <div className="App">
+      <ChatEngine
+        height="100vh"
+        projectID="96c92568-a32a-46c6-8736-4678a69f6060"
+        userName="reross"
+        userSecret="123123"
+        renderChatFeed={(chatAppProps) => <ChatFeed {...chatAppProps} />}
+      />
+    </div>
+  );
+};
 
 export default App;
